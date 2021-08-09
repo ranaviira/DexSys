38a03d461c89c4fd86d5d45db80384f529a1f4b2
ranaviira <84rent@gmail.com>
2021-08-09 08:40:58 +0400

    add corrections2

--- src/company/Group.java
+++ src/company/Group.java
@@ -5,55 +5,70 @@ public class Group {
 
     ArrayList<Integer> list = new ArrayList<>();
 
-    void getArray(String[] numbers) {
+    void getPoint(String[] numbers) {
 
             for (int i = 0; i < numbers.length; i++) {
                 list.add(Integer.parseInt(numbers[i]));
             }
             System.out.println("-----------------------------------------------------------------------");
             System.out.println("Координаты точек успешно введены");
-            System.out.println("-----------------------------------------------------------------------");
     }
 
     void removePoint(int groupNum){
-        double point1 = Math.pow(list.get(0), groupNum) - list.get(1); //x¹-y <=0; groupNum = 1 группа   x²-y <=0; 2 группа    x³-y <=0; 3 группа
-        double point2 = Math.pow(list.get(2), groupNum) - list.get(3);
+        if(list.size() == 4) {
+            double point1 = Math.pow(list.get(0), groupNum) - list.get(1); //x¹-y <=0; groupNum (номер группы совпадает со степенью) = 1 группа  x²-y <=0; 2 группа    x³-y <=0; 3 группа
+            double point2 = Math.pow(list.get(2), groupNum) - list.get(3);
 
-        System.out.println("-----------------------------------------------------------------------");
-        if (point1 <= 0 && point2 <= 0) {
-            list.clear();
-            System.out.println("Из группы " + groupNum + " удалены 2 точки");
+            System.out.println("-----------------------------------------------------------------------");
+            if (point1 <= 0 && point2 <= 0) {
+                list.clear();
+                System.out.println("Удалены координаты 2-х точкек входящие в группу " + groupNum);
+            } else if (point1 <= 0) {
+                list.remove(1);
+                list.remove(0);
+                System.out.println("Удалены координаты 1-ой точки входящей в группу " + groupNum);
+            } else if (point2 <= 0) {
+                list.remove(3);
+                list.remove(2);
+
+                System.out.println("Удалены координаты 1-ой точки входящей в группу " + groupNum);
+            } else {
+                System.out.println("Нет точек входящих в группу " + groupNum);
+            }
         }
-        else if (point1 <= 0) {
-            list.remove(0);
-            list.remove(1);
-            System.out.println("Из группы " + groupNum + " удалена 1 точка");
-        } else if (point2 <= 0) {
-            list.remove(2);
-            list.remove(3);
-            System.out.println("Из группы " + groupNum + " удалена 1 точка");
-        } else {
-            System.out.println("Нет точек входящих в группу " + groupNum);
+        else {
+            System.out.println("Выполните очистку памяти");
         }
+
 
     }
     void printGroup(int groupNum) {
 
-        double point1 = Math.pow(list.get(0), groupNum) - list.get(1); //x¹-y <=0; groupNum = 1 группа   x²-y <=0; 2 группа    x³-y <=0; 3 группа
-        double point2 = Math.pow(list.get(2), groupNum) - list.get(3);
+        if(list.size() == 4) {
+            double point1 = Math.pow(list.get(0), groupNum) - list.get(1); //x¹-y <=0; groupNum = 1 группа   x²-y <=0; 2 группа    x³-y <=0; 3 группа
+            double point2 = Math.pow(list.get(2), groupNum) - list.get(3);
 
-        System.out.println("-----------------------------------------------------------------------");
+            System.out.println("-----------------------------------------------------------------------");
 
-        if (point1 <= 0 && point2 <= 0) {
-            System.out.println("В группу " + groupNum + " входят 2 точки c координатами (" + list.get(0) + "," + list.get(1) + ")(" + list.get(2) + "," + list.get(3) + ")");
-        } else if (point1 <= 0) {
-            System.out.println("В группу " + groupNum + " входит 1 точка c координатами (" + list.get(0) + "," + list.get(1) + ")");
-        } else if (point2 <= 0) {
-            System.out.println("В группу " + groupNum + " входит 1 точка c координатами (" + list.get(2) + "," + list.get(3) + ")");
-        } else {
-            System.out.println("Группа " + groupNum + " пуста");
+            if (point1 <= 0 && point2 <= 0) {
+                System.out.println("В группу " + groupNum + " входят 2 точки c координатами (" + list.get(0) + "," + list.get(1) + ")(" + list.get(2) + "," + list.get(3) + ")");
+            } else if (point1 <= 0) {
+                System.out.println("В группу " + groupNum + " входит 1 точка c координатами (" + list.get(0) + "," + list.get(1) + ")");
+            } else if (point2 <= 0) {
+                System.out.println("В группу " + groupNum + " входит 1 точка c координатами (" + list.get(2) + "," + list.get(3) + ")");
+            } else {
+                System.out.println("Группа " + groupNum + " пуста");
+            }
+
         }
-
+        else if(list.size() > 0){
+            System.out.println("-----------------------------------------------------------------------");
+            System.out.println("Данных больше или меньше чем нужно. Выполните очистку памяти и введите координаты");
+        }
+        else{
+            System.out.println("-----------------------------------------------------------------------");
+            System.out.println("Введите в память программы координаты");
+        }
     }
 }
 
--- src/company/Task.java
+++ src/company/Task.java
@@ -13,12 +13,12 @@ public class Task {
         while (true) {
 
             text.printCommand();
-            int commmand = scanner.nextInt();
 
-            if (commmand == 1) {
+            int command = scanner.nextInt();
 
-                System.out.println("-----------------------------------------------------------------------");
-                System.out.println("Введите в память программы координаты точек через пробел");
+            if (command == 1) {
+
+                System.out.println("Введите в память программы координаты двух точек через пробел");
                 System.out.println("Пример 1 -9 2 3");
                 System.out.println("-----------------------------------------------------------------------");
 
@@ -26,47 +26,55 @@ public class Task {
                 String addPoint = pointScanner.nextLine();
                 String[] numbers = addPoint.split(" ");
 
-                numb.getArray(numbers);
+                numb.getPoint(numbers);
 
             }
-            else if (commmand == 2) {
+            else if (command == 2) {
                 numb.printGroup(1);
                 numb.printGroup(2);
                 numb.printGroup(3);
             }
-
-            else if (commmand == 3) {
+            else if (command == 3) {
                 System.out.println("-----------------------------------------------------------------------");
                 System.out.println("Введите номер группы от 1 до 3");
-                System.out.println("-----------------------------------------------------------------------");
+
                 int inputGroup = scanner.nextInt();
 
-                numb.printGroup(inputGroup);
+                if (inputGroup > 0 && inputGroup <=3) {
+                    numb.printGroup(inputGroup);
+                }
+                else {
+                    System.out.println("-----------------------------------------------------------------------");
+                    System.out.println("Введите номер группы от 1 до 3");
+                }
 
             }
-            else if (commmand == 4) {
+            else if (command == 4) {
+
                 System.out.println("-----------------------------------------------------------------------");
                 System.out.println("Введите номер группы от 1 до 3");
-                System.out.println("-----------------------------------------------------------------------");
+
                 int inputGroup = scanner.nextInt();
 
-                numb.removePoint(inputGroup);
+                if (inputGroup > 0 && inputGroup <=3) {
+                    numb.removePoint(inputGroup);
+                }
+                else {
+                    System.out.println("-----------------------------------------------------------------------");
+                    System.out.println("Введите номер группы от 1 до 3");
+                }
 
-                System.out.println("-----------------------------------------------------------------------");
-                System.out.println("Точки, входящие в группу" + inputGroup + ", удалены");
-                System.out.println("-----------------------------------------------------------------------");
             }
-
-            else if (commmand == 5) {
+            else if (command == 5) {
                 numb.list.clear();
                 System.out.println("-----------------------------------------------------------------------");
                 System.out.println("Память очищена");
                 System.out.println("-----------------------------------------------------------------------");
             }
-            else if (commmand == 6) {
+            else if (command == 6) {
                 text.printHelp();
             }
-            else if (commmand == 7) {
+            else if (command == 7) {
                 System.out.println("-----------------------------------------------------------------------");
                 System.out.println("Выход");
                 System.out.println("-----------------------------------------------------------------------");
@@ -74,9 +82,9 @@ public class Task {
             }
             else {
                 System.out.println("-----------------------------------------------------------------------");
-                System.out.println("Такой команды пока нет. Выберете команду от 1 до 7");}
+                System.out.println("Такой команды пока нет. Выберете команду от 1 до 7");
                 System.out.println("-----------------------------------------------------------------------");
-
+            }
         }
     }
 }
